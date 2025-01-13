94efbae4b714ff3ae19433d2601e28356fb40cf6
j0nathan-l3onardo <jo0o67@hotmail.fr>
2025-01-12 20:46:54 -0600

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,8 +18,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" data-theme="light">
-      <body className={`${roboto.className} flex flex-col items-center min-h-screen bg-base-100`}>
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
+      <body>
         {children}
       </body>
     </html>
