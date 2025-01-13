fd1a9a2e22ddef797e81873260e23e5f50a767de
j0nathan-l3onardo <jo0o67@hotmail.fr>
2025-01-12 20:14:06 -0600

    Remove setup file

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,1 +1,9 @@
-npm install @headlessui/react framer-motion clsx --save
+
+# Install npm packages
+npm install @headlessui/react framer-motion clsx
+
+# Check if npm install was successful
+if [ $? -ne 0 ]; then
+  echo "npm install failed"
+  exit 1
+fi
