6eb20404cf6492d35e2f2d7dfea0e4490eab4070
j0nathan-l3onardo <jo0o67@hotmail.fr>
2025-01-18 13:38:51 -0600

    fix marker

--- app/components/DogMap.tsx
+++ app/components/DogMap.tsx
@@ -2,7 +2,22 @@
 
 import { useEffect } from 'react'
 import { MapContainer, TileLayer, Marker, useMap } from 'react-leaflet'
+import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
+
+// Fix Leaflet default marker icons
+const icon = L.icon({
+  iconUrl: '/images/marker-icon.png',
+  iconRetinaUrl: '/images/marker-icon-2x.png',
+  shadowUrl: '/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+})
+
+L.Marker.prototype.options.icon = icon
 
 interface MapUpdaterProps {
   coordinates: [number, number] | null;
